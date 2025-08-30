bf08066d051911e5ac5ee0e30c78e0e8abed0e36
devbeni <devbeni@noreply.localhost>
2025-08-30 13:09:25 +0200

    Update firka/lib/helpers/ui/firka_button.dart

--- firka/lib/helpers/ui/firka_button.dart
+++ firka/lib/helpers/ui/firka_button.dart
@@ -4,24 +4,41 @@ class FirkaButton extends StatelessWidget {
   final String text;
   final Color bgColor;
   final TextStyle fontStyle;
+  final Icon? icon;
 
   const FirkaButton(
       {required this.text,
       required this.bgColor,
       required this.fontStyle,
+      this.icon,
       super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add shadows
     return Container(
       decoration: BoxDecoration(
         color: bgColor,
         borderRadius: BorderRadius.all(Radius.circular(16)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
       ),
       child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-          child: Text(text, style: fontStyle)),
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child: icon != null
+              ? Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    icon!,
+                    SizedBox(width: 8),
+                    Text(text, style: fontStyle),
+                  ],
+                )
+              : Text(text, style: fontStyle)),
     );
   }
 }
