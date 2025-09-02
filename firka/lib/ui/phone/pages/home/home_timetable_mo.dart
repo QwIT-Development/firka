cbeebf3f0ec55ebba46ae3c20335619037129201
Armand <4831c0@proton.me>
2025-09-02 18:41:07 +0200

    home/*: fix loading spinner

--- firka/lib/ui/phone/pages/home/home_timetable_mo.dart
+++ firka/lib/ui/phone/pages/home/home_timetable_mo.dart
@@ -535,14 +535,15 @@ class _HomeTimetableMonthlyScreen extends State<HomeTimetableMonthlyScreen> {
             ),
           ]));
     } else {
-      return SizedBox(
-        height: MediaQuery.of(context).size.height / 1.35,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      return Scaffold(
+        backgroundColor: appStyle.colors.background,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(),
-            DelayedSpinnerWidget(),
-            SizedBox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [DelayedSpinnerWidget()],
+            )
           ],
         ),
       );
