8a618108042f3e219f571e7b1924b08e20a26a5e
Armand <4831c0@proton.me>
2025-09-13 16:39:31 +0200

    home_subpage: handle back events

--- firka/lib/ui/phone/pages/home/home_subpage.dart
+++ firka/lib/ui/phone/pages/home/home_subpage.dart
@@ -1,13 +1,18 @@
+import 'package:firka/ui/phone/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../helpers/firka_state.dart';
+import '../../../../helpers/update_notifier.dart';
 import '../../../model/style.dart';
 
 class PageWithSubPages extends StatefulWidget {
   final int pageIndex;
   final List<Widget Function(void Function(int))> subPages;
+  final ValueNotifier<bool> subPageActive;
+  final UpdateNotifier back;
 
-  const PageWithSubPages(this.subPages, {Key? key, required this.pageIndex})
+  const PageWithSubPages(this.subPages, this.subPageActive, this.back,
+      {Key? key, required this.pageIndex})
       : super(key: key);
 
   @override
@@ -18,10 +23,42 @@ class _PageWithSubPagesState extends FirkaState<PageWithSubPages> {
   int _currentSubPage = 0;
 
   @override
+  void initState() {
+    super.initState();
+
+    widget.back.addListener(_backListener);
+  }
+
+  void _backListener() {
+    if (!mounted) return;
+
+    setState(() {
+      subPageActive.value = false;
+      _currentSubPage = 0;
+    });
+  }
+
+  @override
+  void didUpdateWidget(PageWithSubPages oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    widget.back.removeListener(_backListener);
+    widget.back.addListener(_backListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    widget.back.removeListener(_backListener);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appStyle.colors.background,
       body: widget.subPages[_currentSubPage]((page) {
+        subPageActive.value = _currentSubPage == 0;
         setState(() {
           _currentSubPage = page;
         });
