b0c8f1f4b34c7855c30ece89f65bf5b62ce5b7de
Armand <4831c0@proton.me>
2026-02-27 22:10:16 +0100

    firka(android): optimize build time

--- firka/android/build.gradle.kts
+++ firka/android/build.gradle.kts
@@ -40,9 +40,6 @@ subprojects {
 
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
